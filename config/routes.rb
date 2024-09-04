38172d464674041d3be1bae66feadb4cbf9ec523
VivekAttada <vivek.attada8@gmail.com>
2024-09-04 15:54:09 +0530

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,22 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  root 'internal_price#index'
-  post '/internal_file_bulk_upload' => 'internal_price#internal_file_bulk_upload', as: 'internal_file_bulk_upload'
-  post '/marketing_price_bulk_upload' => 'internal_price#marketing_price_bulk_upload', as: 'marketing_file_bulk_upload'
-  post '/raw_file_bulk_upload' => 'internal_price#raw_file_bulk_upload', as: 'raw_file_bulk_upload'
-  get '/all_contract_pharmacies' => 'internal_price#all_contract_pharmacies', as: 'all_contract_pharmacies'
-  get '/dashboard' => 'internal_price#dashboard', as: 'dashboard'
-  get '/reimbursement' => 'internal_price#reimbursement', as: 'reimbursement'
-  get '/claim_management' => 'internal_price#claim_management', as: 'claim_management'
-  get '/analytics' => 'internal_price#analytics', as: 'analytics'
-  get '/each_contract_pharmacy' => 'internal_price#each_contract_pharmacy', as: 'each_contract_pharmacy'
+  root to: 'internal_price#index'
+
+  post 'internal_file_bulk_upload', to: 'internal_price#internal_file_bulk_upload', as: 'internal_file_bulk_upload'
+  post 'marketing_price_bulk_upload', to: 'internal_price#marketing_price_bulk_upload', as: 'marketing_file_bulk_upload'
+  post 'raw_file_bulk_upload', to: 'internal_price#raw_file_bulk_upload', as: 'raw_file_bulk_upload'
+  post 'awp_file_bulk_upload', to: 'internal_price#awp_file_bulk_upload', as: 'awp_file_bulk_upload'
+
+  get 'all_contract_pharmacies', to: 'internal_price#all_contract_pharmacies', as: 'all_contract_pharmacies'
+  get 'dashboard', to: 'internal_price#dashboard', as: 'dashboard'
+  get 'reimbursement', to: 'internal_price#reimbursement', as: 'reimbursement'
+  get 'analytics', to: 'internal_price#analytics', as: 'analytics'
+  get 'reimbursement_each_contract_pharmacy', to: 'internal_price#reimbursement_each_contract_pharmacy',
+                                              as: 'reimbursement_each_contract_pharmacy'
+  get 'claim_management', to: 'internal_price#claim_management', as: 'claim_management'
+  get 'claim_each_contract_pharmacy', to: 'internal_price#claim_each_contract_pharmacy',
+                                      as: 'claim_each_contract_pharmacy'
+
+  post 'update_claim_status', to: 'internal_price#update_claim_status', as: 'update_claim_status'
 end
